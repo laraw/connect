6d45b101276512665bbfb92bd04b15d4719fe74d
Lara <s3342496@student.rmit.edu.au>
2015-06-27 22:00:35 +0000

    fixes to sql query

--- answers.php
+++ answers.php
@@ -5,8 +5,14 @@ require_once('./php/helper.php');
 session_start();
 $db = createDBConnection();
 
+// test
+
+
+$searchWine = "archibald";
+
 // get the variables from the Search & trim them 
 
+/*
 $searchWine = addslashes(trim($_GET['wineName'])); // wine name
 $searchWinery = addslashes(trim($_GET['winery']));
 $searchRegion = addslashes(trim($_GET['region']));
@@ -38,6 +44,7 @@ if(!$data) {
 	// $_SESSION['results'] = "Not a valid number!";
 	// header( 'Location: results.php' ) ;
 // }
+*/
 
 // the actual query
 
@@ -49,11 +56,11 @@ $query = "select  GROUP_CONCAT(DISTINCT(gr.variety)) as WineVarieties, w.wine_na
  inner join winery win on w.winery_id = win.winery_id
  inner join region reg on reg.region_id = win.region_id
  inner join (select sum(qty) as totalSold, wine_id 
-						 // from items
-						 // group by wine_id) as wts on wts.wine_id = w.wine_id
+						  from items
+						 group by wine_id) as wts on wts.wine_id = w.wine_id
  inner join (select wine_id, sum(price) as TotalSalesRevenue
-						 // from items
-						 // group by wine_id) as wtsr on wtsr.wine_id = w.wine_id 
+						  from items
+						  group by wine_id) as wtsr on wtsr.wine_id = w.wine_id 
   where 1 = 1";
  
 
@@ -65,54 +72,62 @@ $result = '';
 // build the query based on the parameters
 $query = '';
 
-// complete the query
-"group by  w.wine_name,  w.year, i.cost, i.on_hand, win.winery_name, wts.totalSold, wtsr.TotalSalesRevenue;";
 
 
 // store the query in a session object
-if($searchWine <> '') {
+if($searchWine <> "") {
 	$query = $query . " and w.wine_name like %" . $searchWine . "%";
 }
 
-if($searchWinery <> '') {
+if($searchWinery <> "") {
 	$query = $query . " and win.winery_name like %" . $searchWinery . "%";
 }
 
-if($searchRegion <> '') {
+if($searchRegion <> "") {
 	$query = $query . " and reg.region_name like %" . $searchRegion . "%";
 }
 
-if($searchVariety <> '') {
-	$query = $query . " and reg.region_name like %" . $searchVariety . "%";;
+if($searchVariety <> "") {
+	$query = $query . " and WineVarieties like %" . $searchVariety . "%";
 }
 
-if($searchYear <> '') {
-	$query = $query . " and reg.region_name like %" . $searchYear . "%";;;
+if($searchYear <> "") {
+	$query = $query . " and w.year =" . $searchYear;
+}
+/*
+if($searchMaxOrdered <> "") {
+	
 }
 
-if($searchMaxOrdered <> '') {
+if($searchMinStock <> "") {
 	'';
 }
 
-if($searchMinStock <> '') {
+if($searchMinPrice <> "") {
 	'';
 }
 
-if($searchMinPrice <> '') {
+if($searchMaxPrice <> "") {
 	'';
 }
+*/
 
-if($searchMaxPrice <> '') {
-	'';
-}
+
+// complete the query
+$query = $query . "group by  w.wine_name,  w.year, i.cost, i.on_hand, win.winery_name, wts.totalSold, wtsr.TotalSalesRevenue;";
 
 $rowCount = 0;
-
-$stmt = $db->query($query);
- while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-				$rowCount++;
-                printf("%-40s %-20s\n", $row["title"], $row["author"]);
+try {
+	$stmt = $db->query($query);
+	 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+					$rowCount++;
+					printf("%-40s %-20s\n", $row["wine_name"], $row["winery"]);
+	}
 }
+catch(PDOException e) {
+	echo $e.get_Message();
+}
+
 
 if($rowCount >= 1 && $data) {
 		$_SESSION['results'] = 'Here are the results that match your criteria: ';
