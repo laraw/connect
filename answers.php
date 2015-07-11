4bd54ab2cb1f9221aceed10453450fa4e8eb1d87
Lara <s3342496@student.rmit.edu.au>
2015-07-11 08:23:08 +0000

    added all data validation

--- answers.php
+++ answers.php
@@ -1,26 +1,32 @@
 <?php
 require_once('./php/dataaccess.php');
 //require_once('./php/helper.php');
-
 session_start();
-
 $db = createDBConnection();
 
-
 // if there are any search results from a previous session, clear them out
-
 if (isset($_SESSION['queryRes'])) {
     session_unset($_SESSION['queryRes']);
 }
-
 if (isset($_SESSION['error'])) {
     session_unset($_SESSION['error']);
 }
+/* VALIDATION CHECKS */
+/*
 
+1. Wine name should be a string max 50
+2. Winery name should be string max 100
+3. Min/Max Year min must be less than max, year is int max 4
+4. Min/Max Stock int 5 - min must be less than max
+5. Min/Max Price decimal (5,2) - min must be less than max
+
+User should be returned to search page with the error field highlighted for correction. No results should appear until all errors fixed. */
+$data = true;
+
+// create an array with all the fields in it
+$errors = array();
 
 // get the variables from the Search & trim them 
-
-
 $searchWine       = addslashes(trim($_GET['wineName'])); 
 $searchWinery     = addslashes(trim($_GET['winery']));
 $searchRegion     = addslashes(trim($_GET['region']));
@@ -32,62 +38,81 @@ $searchMinOrdered = addslashes(trim($_GET['minOrdered']));
 $searchMinPrice   = addslashes(trim($_GET['minPrice']));
 $searchMaxPrice   = addslashes(trim($_GET['maxPrice']));
 
-
-
-/* VALIDATION CHECKS */
-/*
-1. Make sure something has been searched for
-2. Wine name should be a string max 50
-3. Winery name should be string max 100
-4. Region should be string max 100 
-5. Variety should be string max 50
-6. Min/Max Year min must be less than max, year is int max 4
-7. Min/Max Stock int 5 - min must be less than max
-8. Min/Max Price decimal (5,2) - min must be less than max
-
-User should be returned to search page with the error field highlighted for correction. No results should appear until all errors fixed.
-
-*/
-
-// create an array with all the fields in it
-$errors = array();
-
-$data = true;
-
-/*removed as per the forum
-foreach ($_GET as $val) {
-    if (strlen($val) > 0 && $val <> 'Submit') {
-        $data = true;
-    }
+// Wine name should be a string max 50  						- $errors['winename']
+if(strlen($searchWine) > 50) {
+	$errors['winename'] = "Wine name is too long - max 50 characters";
+	$data = false;
+}
+elseif ( preg_match("/[^a-z0-9 ]/i", $searchWine) ) {
+	$errors['winename'] = "Wine name contains invalid characters - alphanumeric only";
+	$data = false;
 }
 
-if (!$data) {
-    $errorMsg = "You didn't search for anything!";
+// Winery name should be string max 100							- $errors['wineryname']
+if(strlen($searchWinery) > 100) {
+	$errors['wineryname'] = "Winery name is too long - max 50 characters";
+	$data = false;
 }
-*/
-
-
-
-// Also check that number searches are actually numeric 
-if ((!is_numeric($searchMinStock) && $searchMinStock <> '') || (!is_numeric($searchMaxOrdered) && $searchMaxOrdered <> '') || (!is_numeric($searchMinPrice) && $searchMinPrice <> '') || (!is_numeric($searchMaxPrice) && $searchMaxPrice <> '')) {
-    $data     = false;
-    $errorMsg = "Not a valid number!";
-    
+elseif ( preg_match("/[^a-z0-9 ]/i", $searchWinery) ) {
+	$errors['wineryname'] = "Winery name contains invalid characters - alphanumeric only";
+	$data = false;
 }
 
-
-// make sure the minimum year isn't greater than the maximum year
-
+// Min/Max Year min must be less than max, year is int max 4	- $errors['year']
 if ($searchMinYear > $searchMaxYear) {
-    $data     = false;
-    $errorMsg = "Your minimum year is greater than your maximum!";
-    
+    $errors['year'] = "Your minimum year is greater than your maximum!";
+	$data = false;
 }
 
-// length of string and data types
+// Min Stock int 5 digits only	0-10000		-  $errors['stock']
+if (!is_numeric($searchMinStock) && $searchMinStock <> '') {
+	$errors['stock'] = "Not a number";
+	$data = false;
+}
+elseif(fmod($searchMinStock, 1) <> 0.0) {
+	$errors['stock'] = "Must be a whole number!";
+	$data = false;
+}
+elseif($searchMinStock > 99999 || $searchMinStock < 0 ) {
+	$errors['stock'] = "Must be a number between 0 and 10,000";
+	$data = false;
+}
 
+// Min Ordered int 0 or larger
+if (!is_numeric($searchMinOrdered) && $searchMinOrdered <> '') {
+	$errors['ordered'] = "Not a number";
+	$data = false;
+}
+elseif(fmod($searchMinOrdered, 1) != 0.0) {
+	$errors['ordered'] = "Must be a whole number!";
+	$data = false;
+}
+elseif($searchMinOrdered < 0 ) {
+	$errors['ordered'] = "Must not be a negative number";
+	$data = false;
+}
 
-
+// Min/Max Price decimal (5,2) - min must be less than max		
+$priceErr = array();
+$priceData = true;
+if((preg_match("/\A(?!\d{1,5}(?:\.\d{1,2})?\z).*/", $searchMinPrice)) && $searchMinPrice <> '') {
+	$priceErr[] = "Min Price invalid format";
+	$priceData = false;
+}
+if((preg_match("/\A(?!\d{1,5}(?:\.\d{1,2})?\z).*/", $searchMaxPrice)) && $searchMaxPrice <> '') {
+	$priceErr[] = "Max Price invalid format";
+	$priceData = false;
+}
+if($searchMinPrice <> '' && $searchMaxPrice <> '') {
+	if($priceData && $searchMinPrice >= $searchMaxPrice ) {
+		$priceErr[] = "Min Price must be less than Max Price";
+		$priceData = false;
+	}
+}
+if(!$priceData) {
+	$errors['price'] = implode(" - ", $priceErr);
+	$data = false;
+}
 // check for ascii code or malicious code
 
 // check the data type & length matches the length in the database see
@@ -99,22 +124,14 @@ $errorMsg = "Not a valid number!";
 }
 */
 
-
-
-
-
 // If the validation check fails ... return to the previous page with error message 
-
 if (!$data) {
-    $_SESSION['error'] = $errorMsg;
+    $_SESSION['error'] = $errors;
     header('Location: search.php');
     exit;
 }
 
-
 /* BUILD SEARCH QUERY */
-
-
 $query = "select  GROUP_CONCAT(DISTINCT(gr.variety)) as WineVarieties, w.wine_name as WineName,  
 		 w.year as Year, min(i.cost) as Cost, i.on_hand as Stock, win.winery_name as Winery, coalesce(TotalSold,0.00) as TotalSold, 
 		  coalesce(TotalSalesRevenue,0.00) as TotalSalesRevenue, 
@@ -128,75 +145,50 @@ $query = "select  GROUP_CONCAT(DISTINCT(gr.variety)) as WineVarieties, w.wine_na
 		 left join (select sum(qty) as totalSold,sum(price) as TotalSalesRevenue, wine_id 
 								  from items
 								 group by wine_id) as wts on wts.wine_id = w.wine_id
-		  where 1 = 1";
-
+		 where 1 = 1";
 
 // set the rowcount 
-
 $rowCount = 1;
 $result   = '';
 
 // build the query based on the parameters
-
-
 if ($searchWine <> "") {
     $query = $query . " and w.wine_name like " . "'%" . $searchWine . "%'";
 }
-
-
 if ($searchWinery <> "") {
     $query = $query . " and win.winery_name like " . "'%" . $searchWinery . "%'";
 }
-
 if ($searchRegion <> "" and $searchRegion <> "All") {
     $query = $query . " and reg.region_name like " . "'%" . $searchRegion . "%'";
 }
-
 if ($searchVariety <> "") {
     $query = $query . " and gr.variety like " . "'%" . $searchVariety . "%'";
 }
-
 if ($searchMinYear <> "") {
     $query = $query . " and w.year >= " . "'" . $searchMinYear . "'";
 }
-
 if ($searchMaxYear <> "") {
     $query = $query . " and w.year <= " . "'" . $searchMaxYear . "'";
 }
-
-
 if ($searchMinOrdered <> "") {
     $query = $query . " and totalSold >= " . $searchMinOrdered;
 }
-
-
 if ($searchMinStock <> "") {
     $query = $query . " and i.on_hand >= " . $searchMinStock;
 }
-
-
 if ($searchMinPrice <> "") {
     $query = $query . " and i.cost >= " . $searchMinPrice;
 }
-
 if ($searchMaxPrice <> "") {
     $query = $query . " and i.cost <= " . $searchMaxPrice;
 }
 
-
-
-
 // complete the query
 $query = $query . " group by  w.wine_id;";
-
-
-
 $rowCount = 0;
 
 
 // store the query in a session object
-
-
 try {
     $stmt = $db->prepare($query);
     $stmt->execute();
@@ -210,7 +202,6 @@ catch (PDOException $e) {
     echo $e->getMessage();
 }
 
-
 if ($rowCount >= 1 && $data) {
     $_SESSION['results'] = 'There were ' . $rowCount . ' results that match your criteria: ';
 }
@@ -219,8 +210,5 @@ if ($rowCount < 1 && $data) {
     $_SESSION['results'] = 'Sorry there were no results matching your criteria!';
 }
 
-$sessID = session_id();
-
-header('Location: results.php?PHPSESSID=' . $sessID);
-
+header('Location: results.php');
 ?>
--- search.php
+++ search.php
@@ -7,20 +7,30 @@
 <?php
 session_start();
 if (isset($_SESSION['error'])) {
-    echo '<p><b>' . $_SESSION['error'] . '</p></b>';
+	//echo '<p><b>' . var_dump($_SESSION['error']) . '</p></b>'; debug
+	echo '<p><span id="error"> Errors Present please re-enter data</span></p>';
 }
-
 else {
     echo '<p> Please select your search parameters: </p>';
 }
 
 ?>
 <form action="answers.php" method="GET">
-<h4> Wine Name: </h4>
+<h4> Wine Name: </h4> 
+<?php 
+if(isset($_SESSION['error']['winename'])) {
+	echo '<span id="error">' . $_SESSION['error']['winename'] . '</span></br>';
+}
+?>
 <input type="text" id="wineName" name="wineName">
 <br />
 <br />
 <h4> Winery Name: </h4>
+<?php 
+if(isset($_SESSION['error']['wineryname'])) {
+	echo '<span id="error">' . $_SESSION['error']['wineryname'] . '</span></br>';
+}
+?>
 <input type="text" id="winery" name="winery">
 <br />
 <br />
@@ -31,9 +41,7 @@ else {
 include_once('./php/dataaccess.php');
 
 $db = createDBConnection();
-
 $regions = getRegions($db);
-
 
 echo '<select id="region" name="region"> <option value=""> </option>';
 
@@ -43,42 +51,38 @@ foreach ($regions as $region) {
 }
 echo '</select>';
 ?>
-
 <br />
 <br />
 <h4> Grape Variety </h4>
 <?php
 // get the grape variety data from the database
-
 include_once('./php/dataaccess.php');
 $db = createDBConnection();
-
 $varieties = getVariety($db);
-
-
 echo '<select id="variety" name="variety">';
 echo '<option value=""> </option>';
+
 /* Populate the grape variety list */
 foreach ($varieties as $variety) {
     echo '<option value="' . $variety . '">' . $variety . '</option>';
 }
-
 echo '</select>';
 ?>
 <br />
 <br />
 <h4> Year </h4>
+<?php 
+if(isset($_SESSION['error']['year'])) {
+	echo '<span id="error">' . $_SESSION['error']['year'] . '</span></br>';
+}
+?>
 Min: 
 <?php
 // get the year data from the database
 $years = array();
-
 include_once('./php/dataaccess.php');
 $db = createDBConnection();
-
 $years = getYearRange($db);
-
-
 echo '<select id="minyear" name="minyear">';
 echo '<option value=""> </option>';
 
@@ -92,12 +96,9 @@ Max:
 <?php
 // get the year data from the database
 $years = array();
-
 include_once('./php/dataaccess.php');
 $db = createDBConnection();
-
 $years = getYearRange($db);
-
 echo '<select id="maxyear" name="maxyear">';
 echo '<option value=""> </option>';
 
@@ -106,20 +107,36 @@ foreach ($years as $year) {
     echo '<option value="' . $year . '">' . $year . '</option>';
 }
 echo '</select>';
-
-
 ?>
 <br />
 <br />
 <h4> Min Stock </h4>
+<?php 
+if(isset($_SESSION['error']['stock'])) {
+	echo '<span id="error">' . $_SESSION['error']['stock'] . '</span></br>';
+}
+?>
 <input type="text" id="minStock" name="minStock" size="10">
 <br />
 <br />
 <h4> Min Ordered </h4>
+<?php 
+if(isset($_SESSION['error']['ordered'])) {
+	echo '<span id="error">' . $_SESSION['error']['ordered'] . '</span></br>';
+}
+?>
 <input type="text" id="maxOrdered" name="minOrdered" size="10">
 <br />
 <br />
 <h4> Price Range </h4>
+<?php 
+if(isset($_SESSION['error']['price'])) {
+	echo '<span id="error">' . $_SESSION['error']['price'] . '</span></br>';
+}
+if (isset($_SESSION['error'])) {
+ unset($_SESSION['error']);
+}
+?>
 Min:
 <input type="text" id="minPrice" name="minPrice" size="10">
 Max:
@@ -128,6 +145,5 @@ Max:
 <br />
 <input type="submit" name="submit">
 </form>
-
 </body>
 </html>
